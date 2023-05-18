909da3752b826c07e0424f874a5bcad18482061b
Brandon Cohen <brandon.f.cohen@gmail.com>
2023-05-17 21:26:47 -0400

    remove mongo

--- src/pages/api/submitDrawing.ts
+++ src/pages/api/submitDrawing.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 //import { MongoClient } from "mongodb";
 import sgMail from "@sendgrid/mail"; // Import SendGrid Mail
 
-const mongoConnectionString =
-  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017";
+// const mongoConnectionString =
+//   process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017";
 
 sgMail.setApiKey(process.env.MLG_SENDGRID || ""); // Set SendGrid API Key
 
